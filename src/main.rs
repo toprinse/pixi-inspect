48d5d89fe46c28d409d0353a6ecaf1fc56bafc44
Pavel Zwerschke <pavelzw@gmail.com>
2025-10-18 21:40:29 +0900

    Don't write package to temporary file when reading from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
-use clap::{Parser, Subcommand};
 use anyhow::Result;
+use clap::{Parser, Subcommand};
+use rattler_conda_types::package::{ArchiveType, IndexJson, PackageFile};
+use rattler_package_streaming::seek::{self as rattler_seek, read_package_file_content};
 use std::path::PathBuf;
-use std::env;
-use std::time::SystemTime;
 use tokio::io::AsyncReadExt;
-use tokio::fs;
-use rattler_conda_types::package::IndexJson;
-use rattler_package_streaming::seek as rattler_seek;
 
 #[derive(Parser)]
 #[command(name = "pixi-inspect")]
@@ -32,33 +29,38 @@ async fn main() -> Result<()> {
     match cli.command {
         Commands::GetInfo { path } => {
             // Resolve input: file on disk or read stdin to a temp file
-            let input_path: PathBuf;
-            let mut remove_after = false;
-            if path == "-" {
+            let index_json: IndexJson = if path == "-" {
                 let mut stdin = tokio::io::stdin();
                 let mut buf = Vec::new();
                 stdin.read_to_end(&mut buf).await?;
-
-                // Create a temporary file with a unique identifier
-                let nanos = SystemTime::now().duration_since(std::time::UNIX_EPOCH)?;
-                let tmp_name = format!("pixi-inspect-{}.conda", nanos.as_nanos());
-                let tmp_path = env::temp_dir().join(tmp_name);
-                fs::write(&tmp_path, &buf).await?;
-                input_path = tmp_path;
-                remove_after = true;
+                let magic_bytes = &buf[0..4];
+                // https://en.wikipedia.org/wiki/List_of_file_signatures
+                let archive_type = match magic_bytes {
+                    // ZIP magic number (PK) - Modern conda format
+                    [0x50, 0x4B, 0x03, 0x04]
+                    | [0x50, 0x4B, 0x05, 0x06]
+                    | [0x50, 0x4B, 0x07, 0x08] => ArchiveType::Conda,
+                    // Gzip magic number - Legacy format
+                    [0x1f, 0x8b, _, _] => ArchiveType::TarBz2,
+                    _ => {
+                        return Err(anyhow::anyhow!(
+                            "Unsupported archive type. Magic bytes {magic_bytes:?} don't match any known format"
+                        ));
+                    }
+                };
+                let mut reader = std::io::Cursor::new(buf);
+                let content = read_package_file_content(
+                    &mut reader,
+                    archive_type,
+                    IndexJson::package_path(),
+                )?;
+                IndexJson::from_str(&String::from_utf8_lossy(&content))?
             } else {
-                input_path = PathBuf::from(path);
-            }
-
-            let index_json: IndexJson = rattler_seek::read_package_file(&input_path)?;
+                rattler_seek::read_package_file(&PathBuf::from(path))?
+            };
 
             // Display the JSON
             println!("{}", serde_json::to_string_pretty(&index_json)?);
-
-            // Cleanup temp file if used
-            if remove_after {
-                let _ = fs::remove_file(input_path).await;
-            }
 
             Ok(())
         }
