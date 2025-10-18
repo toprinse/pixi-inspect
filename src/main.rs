ccf45b048feee7ed232ed505fea69189129f5f74
Pavel Zwerschke <pavelzw@gmail.com>
2025-10-18 21:42:43 +0900

    remove unneeded dependencies

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ use anyhow::Result;
 use clap::{Parser, Subcommand};
 use rattler_conda_types::package::{ArchiveType, IndexJson, PackageFile};
 use rattler_package_streaming::seek::{self as rattler_seek, read_package_file_content};
-use std::path::PathBuf;
-use tokio::io::AsyncReadExt;
+use std::{io::Read, path::PathBuf};
 
 #[derive(Parser)]
 #[command(name = "pixi-inspect")]
@@ -22,17 +21,16 @@ enum Commands {
     },
 }
 
-#[tokio::main]
-async fn main() -> Result<()> {
+fn main() -> Result<()> {
     let cli = Cli::parse();
 
     match cli.command {
         Commands::GetInfo { path } => {
             // Resolve input: file on disk or read stdin to a temp file
             let index_json: IndexJson = if path == "-" {
-                let mut stdin = tokio::io::stdin();
+                let mut stdin = std::io::stdin();
                 let mut buf = Vec::new();
-                stdin.read_to_end(&mut buf).await?;
+                stdin.read_to_end(&mut buf)?;
                 let magic_bytes = &buf[0..4];
                 // https://en.wikipedia.org/wiki/List_of_file_signatures
                 let archive_type = match magic_bytes {
